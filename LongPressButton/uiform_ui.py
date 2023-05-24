f8626e437006e5bb1fab22a941c29b1d5096338a
Eli-wk1988 <78833528+Eli-wk1988@users.noreply.github.com>
2023-05-24 15:05:20 +0800

    Add files via upload

---
+++ LongPressButton/uiform_ui.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'uiform.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        #self.pushButton = QPushButton(Form)
+        #self.pushButton.setObjectName(u"pushButton")
+        #self.pushButton.setGeometry(QRect(140, 100, 150, 51))
+        #font = QFont()
+        #font.setPointSize(12)
+        #self.pushButton.setFont(font)
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        #self.pushButton.setText(QCoreApplication.translate("Form", u"\u6d4b\u8bd5\u6309\u94ae", None))
+    # retranslateUi
+
